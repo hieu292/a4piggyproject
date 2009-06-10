6e498da9cf15af9c136bc1c203db9e0d0c02de2c
vinhpham.it <vinhpham.it@gmail.com>
2009-06-10 01:17:53 +0000

    dung co doi thu muc lung tung nua Loi tum lum

---
+++ Sources/A4_Project/A4_Project/frmSubmitAssignment.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace A4_Project
+{
+    partial class frmSubmitAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTenMonHoc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lklblFileNop = new System.Windows.Forms.LinkLabel();
+            this.txtLinkFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.opnFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lblTenMonHoc
+            // 
+            this.lblTenMonHoc.AutoSize = true;
+            this.lblTenMonHoc.Location = new System.Drawing.Point(15, 25);
+            this.lblTenMonHoc.Name = "lblTenMonHoc";
+            this.lblTenMonHoc.Size = new System.Drawing.Size(76, 13);
+            this.lblTenMonHoc.TabIndex = 0;
+            this.lblTenMonHoc.Text = "[Tên môn học]";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "[Tiêu đề]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "[Ngày Deadline]";
+            // 
+            // lklblFileNop
+            // 
+            this.lklblFileNop.AutoSize = true;
+            this.lklblFileNop.Location = new System.Drawing.Point(491, 25);
+            this.lklblFileNop.Name = "lklblFileNop";
+            this.lklblFileNop.Size = new System.Drawing.Size(65, 13);
+            this.lklblFileNop.TabIndex = 1;
+            this.lklblFileNop.TabStop = true;
+            this.lklblFileNop.Text = "[linkFileNop]";
+            this.lklblFileNop.Visible = false;
+            // 
+            // txtLinkFile
+            // 
+            this.txtLinkFile.Location = new System.Drawing.Point(29, 84);
+            this.txtLinkFile.Name = "txtLinkFile";
+            this.txtLinkFile.Size = new System.Drawing.Size(394, 20);
+            this.txtLinkFile.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(437, 81);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse..";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(526, 81);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // frmSubmitAssignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 136);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtLinkFile);
+            this.Controls.Add(this.lklblFileNop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTenMonHoc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSubmitAssignment";
+            this.Text = "Nộp Bài";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTenMonHoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel lklblFileNop;
+        private System.Windows.Forms.TextBox txtLinkFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.OpenFileDialog opnFileDlg;
+    }
+}
---
+++ Sources/A4_Project/DAO/DataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DAO
+{
+    class DataProvider
+    {
+        public static OleDbConnection ConnectionData()
+        {
+            string cnStr = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + Application.StartupPath + "\\ACCESSMOODLE.mdb";  
+            OleDbConnection cn = new OleDbConnection(cnStr);
+            cn.Open();
+            return cn;
+        }
+    }
+}
